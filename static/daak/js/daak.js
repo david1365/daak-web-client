23e0b0fdd1a9465b09f288f8fa01f30ec76e2ac5
d.akbari <daak1365@yahoo.com>
2018-01-23 18:57:02 +0330

    Complete daak library

--- static/daak/js/daak.js
+++ static/daak/js/daak.js
@@ -8,6 +8,13 @@ var daak = (function ()
         return daakObj;
     };
 
+    daak.elId = 0;
+    // var addMethods = function (elem) {
+    //     elem.forEach(function (item, inex) {
+    //         alert(item)
+    //     })
+    // }
+
     //Define daak’s fn prototype, specially contains init method
     daak.fn = daak.prototype = {
         init: function (selector, context)
@@ -16,24 +23,35 @@ var daak = (function ()
             {
                 return this;
             }
-            if (typeof selector == 'string') {
-                if (selector.startsWith("<")) {
-                    daak.htmlDom(selector)
+
+            if ( typeof selector === "string" ) {
+                if ( selector[ 0 ] === "<" &&
+                    selector[ selector.length - 1 ] === ">" &&
+                    selector.length >= 3 ) {
+
+                    this[0] = daak.parseHTML(selector);
                 }
                 else{
-                    return document.querySelectorAll(selector);
+                    this[0] = document.querySelectorAll(selector);
                 }
             }
             else {
-                return selector;
+                this[0] = selector;
             }
+
+            if (this[0]){
+                // addMethods(this[0]);
+                this.length = 1;
+            }
+
+            return this;
         }
     };
 
     // Give the init function the "daak" prototype for later instantiation
     daak.fn.init.prototype = daak.fn;
 
-    daak.htmlDom = function(string) {
+    daak.parseHTML = function(string) {
         var parser = new DOMParser(),
             content = 'text/html',
             DOM = parser.parseFromString(string, content);
@@ -42,13 +60,75 @@ var daak = (function ()
         return DOM.body.childNodes[0];
     }
 
-    daak.fn.addEventListener = function (type,listener) {
-        if (document.addEventListener) {                // For all major browsers, except IE 8 and earlier
-            daak(this).addEventListener(type, listener);
-        } else if (document.attachEvent) {              // For IE 8 and earlier versions
-            daak(this).attachEvent("on" + type, listener);
+    var run = function (tags) {
+        for(var objectName in window){
+            var object = window[objectName];
+            if(object){
+                if(object.render){
+                    for(var i = 0; i < tags.length; i++){
+                        if(tags[i].tagName == objectName.toUpperCase()) {
+                            var daakId = 'daak-' + objectName + '-' + daak.elId.toString();
+
+                            daak[daakId] = daak(object.render);
+                            daak[daakId].setAttribute('daak-id', daakId);
+
+                            // window[daak.elId].dom = window[daak.elId];
+                            daak[daakId].textChange = object.textChange;
+
+                            var input = daak[daakId].querySelectorAll('input:first-child')[0];
+                             // var ev = objectName + '.' + input.getAttribute('onchange') + '(e);'
+
+                            daak(input).addEventListener('keyup', function (e) {
+                                // eval(ev);
+                                var target = e.target;alert(daak(target).daakElement)
+                                daak(target).daakElement[target.getAttribute('onchange')](e);
+
+                                alert(daakId);
+                            });
+
+                            input.removeAttribute('onchange');
+
+
+                            tags[i].parentNode.replaceChild(daak[daakId][0], tags[i]);
+                            daak.elId++;
+                        }
+                    }
+                }
+            }
         }
     }
+
+    daak.fn.daakElement = function () {
+        var elem = this[0];
+        while ( ( elem = elem[ "parentNode" ] ) && elem.nodeType !== 9 ) {
+            if ( elem.nodeType === 1 ) {
+                if ( elem.getAttribute('daak-id') ) {
+                    return elem;
+                }
+            }
+        }
+    }
+
+    daak.fn.addEventListener = function (type,listener) {
+        if (document.addEventListener) {                // For all major browsers, except IE 8 and earlier
+            this[0].addEventListener(type, listener);
+        } else if (document.attachEvent) {              // For IE 8 and earlier versions
+            this[0].attachEvent("on" + type, listener);
+        }
+    }
+
+    daak.fn.setAttribute = function(name,value){
+        this[0].setAttribute(name, value)
+    }
+
+    daak.fn.querySelectorAll = function(selector){
+        return this[0].querySelectorAll(selector)
+    }
+
+    //start daak
+    daak(document).addEventListener('DOMContentLoaded', function() {
+        run(document.getElementsByTagName('*'));
+    });
 
     // Return "daak" to the global object
     return daak;
